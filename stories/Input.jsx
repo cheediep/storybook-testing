bb697a53e1aba871a71fab7e78f9c844195e33e0
Chee <c.diep@hotmail.com>
2024-09-30 16:01:55 +0100

    Add new story test

---
+++ stories/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Input = () => {
+  return (
+    <input type="text"/>
+  );
+};
